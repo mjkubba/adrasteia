b043888e96a3e24c71ada17e7508e1004ac0cd2c
MJ Kubba <mjkubba@gmail.com>
2019-03-24 17:35:15 -0500

    updating few more things and removing some others

--- client/index.js
+++ client/index.js
@@ -4,17 +4,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import Tf from './components/data/data.js';
+import Data from './components/data/data.js';
 import Main from './components/main/main.js';
 
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
 
 render(
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
         <IndexRoute component={Main} />
-      <Route path="/data" component={Tf} />
+      <Route path="/data" component={Data} />
     </Route>
   </Router>
   , document.getElementById('root'));
